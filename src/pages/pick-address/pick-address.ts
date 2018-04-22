2b09a8e7a238d74848a63931e50e4333410d651e
Léo Candido Silva <leocansil@gmail.com>
2018-04-22 14:46:53 -0300

    Armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/domain/cart.service';
+import { PedidoDTO } from './../../models/pedido.dto';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { StorageService } from './../../services/storage.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
@@ -13,11 +15,14 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
+  pedido: PedidoDTO;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -26,6 +31,15 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {id : response['id']},
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(x => { return {quantidade: x.quantidade, produto: {id: x.produto.id}}})
+          };
         }, error => {
           if(error.status == 403) {
             this.navCtrl.setRoot('HomePage');
@@ -36,4 +50,8 @@ export class PickAddressPage {
     }
   }
 
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = { id: item.id };
+    console.log(this.pedido);
+  }
 }
